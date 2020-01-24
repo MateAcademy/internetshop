5aa22e9ca503c1e5ae181f9c11ddbfe183a2b24a
Sergey Klunniy <s.klunniy@gmail.com>
2020-01-24 03:28:43 +0200

    some changes 24.01.2020

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,18 +1,117 @@
-ALTER SCHEMA `shop`
-DEFAULT CHARACTER SET utf8
-DEFAULT COLLATE utf8_general_ci;
+CREATE SCHEMA `shop` DEFAULT CHARACTER SET utf8 ;
 
-USE `shop`;
+CREATE TABLE `shop`.`items` (
+  `item_id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `price` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(45) NULL,
+  PRIMARY KEY (`item_id`));
 
-CREATE TABLE `items` (
-  `item_id`     int NOT NULL AUTO_INCREMENT,
-  `name`        varchar(45)  DEFAULT NULL,
-  `price`       double       DEFAULT NULL,
-  `description` varchar(45)  DEFAULT NULL,
-  PRIMARY KEY (`item_id`)
-);
+CREATE TABLE `shop`.`orders` (
+  `order_id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `user_id` BIGINT(20) NOT NULL,
+  PRIMARY KEY (`order_id`));
+  
+  CREATE TABLE `shop`.`baskets` (
+  `basket_id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `user_id` BIGINT(20) NULL,
+  PRIMARY KEY (`basket_id`));
+  
+  CREATE TABLE `shop`.`users` (
+  `user_id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `surname` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NULL,
+  `phone` VARCHAR(45) NULL,
+  `login` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `token` VARCHAR(45) NULL,
+  PRIMARY KEY (`user_id`));
 
-insert into shop.items
+CREATE TABLE `shop`.`roles` (
+  `role_id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `role_name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`role_id`));
+  
+  CREATE TABLE `shop`.`users_roles` (
+  `users_roles_id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `user_id` BIGINT(20) NOT NULL,
+  `role_id` BIGINT(20) NOT NULL,
+  PRIMARY KEY (`users_roles_id`),
+  INDEX `user_id_fk_idx` (`user_id` ASC) VISIBLE,
+  INDEX `role_id_fk_idx` (`role_id` ASC) VISIBLE,
+  CONSTRAINT `user_id_fk`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `shop`.`users` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `role_id_fk`
+    FOREIGN KEY (`role_id`)
+    REFERENCES `shop`.`roles` (`role_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+	
+	
+	INSERT INTO `shop`.`users` (`user_id`, `name`, `surname`, `email`, `phone`, `login`, `password`, `token`) VALUES ('1', 'Sergey', 'Klunniy', 'ava@gmail.com', '+380501430700', 'ava', '1', 'eee-rrr-ttt');
+	
+CREATE TABLE `shop`.`items_baskets` (
+  `items_baskets_id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `basket_id` BIGINT(20) NULL,
+  `item_id` BIGINT(20) NULL,
+  PRIMARY KEY (`items_baskets_id`),
+  INDEX `bask_id_fk_idx` (`basket_id` ASC) VISIBLE,
+  INDEX `item_id_fk_idx` (`item_id` ASC) VISIBLE,
+  CONSTRAINT `basket_id_fk`
+    FOREIGN KEY (`basket_id`)
+    REFERENCES `shop`.`baskets` (`basket_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `item_id_fk`
+    FOREIGN KEY (`item_id`)
+    REFERENCES `shop`.`items` (`item_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+	
+	ALTER TABLE `shop`.`orders` 
+ADD INDEX `order_id_fk_idx` (`user_id` ASC) VISIBLE;
+;
+ALTER TABLE `shop`.`orders` 
+ADD CONSTRAINT `order_id_fk`
+  FOREIGN KEY (`user_id`)
+  REFERENCES `shop`.`users` (`user_id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+CREATE TABLE `shop`.`orders_items` (
+  `orders_items_id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `item_id` BIGINT(20) NULL,
+  `order_id` BIGINT(20) NULL,
+  PRIMARY KEY (`orders_items_id`),
+  INDEX `item_id_fk_idx` (`item_id` ASC) VISIBLE,
+  INDEX `order_id_fk_idx` (`order_id` ASC) VISIBLE,
+  CONSTRAINT `item_fk`
+    FOREIGN KEY (`item_id`)
+    REFERENCES `shop`.`items` (`item_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `order_fk`
+    FOREIGN KEY (`order_id`)
+    REFERENCES `shop`.`orders` (`order_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+	
+	ALTER TABLE `shop`.`baskets` 
+CHANGE COLUMN `user_id` `user_id` BIGINT(20) NULL ,
+ADD INDEX `user_id_fk_idxx` (`user_id` ASC) VISIBLE;
+;
+ALTER TABLE `shop`.`baskets` 
+ADD CONSTRAINT `basket_fk_users`
+  FOREIGN KEY (`user_id`)
+  REFERENCES `shop`.`users` (`user_id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+  
+  insert into shop.items
 values ('1', 'car', '900', 'made in Ukraine');
 insert into shop.items
 values ('2', 'car2', '1100', 'made in Ukraine');
@@ -23,73 +122,8 @@ values ('4', 'car4', '1500', 'made in France');
 insert into shop.items
 values ('5', 'car5', '13600', 'made in Russia');
 
-ALTER TABLE `shop`.`items`
-  CHANGE COLUMN `name` `name` VARCHAR(45) CHARACTER SET 'utf8mb4'
-COLLATE 'utf8mb4_0900_ai_ci' NOT NULL,
-  CHANGE COLUMN `price` `price` DECIMAL(10, 0) NOT NULL,
-  CHANGE COLUMN `description` `description` VARCHAR(1000) CHARACTER SET 'utf8mb4'
-COLLATE 'utf8mb4_0900_ai_ci' NULL;
+INSERT INTO `shop`.`users` (`name`, `surname`,`email`, `phone`,`login`, `password`, `token`)
+VALUES ('Serge', 'Klunniy', 'ava@gmail.com', '555', 'ava', '1', 'rrr-ttt');
 
-CREATE TABLE `shop`.`orders` (
-  `order_id` INT NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`order_id`)
-);
-
-CREATE TABLE `shop`.`orders_items` (
-  `idorders_items_id` INT NOT NULL AUTO_INCREMENT,
-  `order_id`          INT NOT NULL,
-  `item_id`           INT NOT NULL,
-  PRIMARY KEY (`idorders_items_id`),
-  INDEX `orders_items_orders_fl_idx` (`order_id` ASC) VISIBLE,
-  INDEX `orders_items_items_fk_idx` (`item_id` ASC) VISIBLE,
-  CONSTRAINT `orders_items_orders_fl`
-  FOREIGN KEY (`order_id`)
-  REFERENCES `shop`.`orders` (`order_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `orders_items_items_fk`
-  FOREIGN KEY (`item_id`)
-  REFERENCES `shop`.`items` (`item_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-);
-
-ALTER TABLE `shop`.`orders`
-  ADD COLUMN `user_id` INT NOT NULL
-  AFTER `order_id`,
-  ADD INDEX `ordrs_users_fk_idx` (`user_id` ASC) VISIBLE;
-;
-ALTER TABLE `shop`.`orders`
-  ADD CONSTRAINT `ordrs_users_fk`
-FOREIGN KEY (`user_id`)
-REFERENCES `shop`.`users` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-
-INSERT INTO `shop`.`orders` (`order_id`, `user_id`) VALUES ('1', '1');
-INSERT INTO `shop`.`orders` (`order_id`, `user_id`) VALUES ('2', '1');
-INSERT INTO `shop`.`orders` (`order_id`, `user_id`) VALUES ('3', '2');
-
-INSERT INTO `shop`.`orders_items` (`idorders_items_id`, `order_id`, `item_id`) VALUES ('1', '1', '1');
-INSERT INTO `shop`.`orders_items` (`idorders_items_id`, `order_id`, `item_id`) VALUES ('2', '1', '2');
-INSERT INTO `shop`.`orders_items` (`idorders_items_id`, `order_id`, `item_id`) VALUES ('3', '2', '4');
-INSERT INTO `shop`.`orders_items` (`idorders_items_id`, `order_id`, `item_id`) VALUES ('4', '3', '2');
-INSERT INTO `shop`.`orders_items` (`idorders_items_id`, `order_id`, `item_id`) VALUES ('5', '3', '5');
-
-ALTER TABLE `shop`.`users`
-ADD COLUMN `bucket_id` VARCHAR(45) NULL AFTER `salt`,
-CHANGE COLUMN `roles` `salt` VARCHAR(45) NULL ;
-
-CREATE TABLE `shop`.`user_roles` (
-  `iduser_roles_id` INT NOT NULL AUTO_INCREMENT,
-  `user_id` VARCHAR(45) NULL,
-  `role_id` VARCHAR(45) NULL,
-  PRIMARY KEY (`iduser_roles_id`));
-
-CREATE TABLE `shop`.`roles` (
-  `role_id` INT NOT NULL AUTO_INCREMENT,
-  `role_name` VARCHAR(45) NULL,
-  PRIMARY KEY (`role_id`));
-
-INSERT INTO `shop`.`roles` (`role_id`, `role_name`) VALUES ('1', 'ADMIN');
-INSERT INTO `shop`.`roles` (`role_id`, `role_name`) VALUES ('2', 'USER');
+INSERT INTO `shop`.`users` (`name`, `surname`,`email`, `phone`,`login`, `password`, `token`)
+VALUES ('Serge2', 'Klunniy2', 'ava2@gmail.com', '555', 'admin', '1', 'rrr-ttt');
